c0a377fbb150ca1d1d0a4228bda2114421580b92
FraserSingh <fraser.singh@hotmail.com>
2026-02-21 19:00:31 +0000

    feat: scripts to manage db entries

---
+++ scripts/manual reset admin pass.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash
+
+from app import create_app
+from app.extensions import db
+from app.models import Team, User
+
+app = create_app()
+app.app_context().push()
+admin = User.query.filter_by(email="user@example.com").first()
+if admin:
+    db.session.delete(admin)
+    db.session.commit()
+
+team = Team.query.first()  # or create a new team if needed
+
+new_admin = User(
+    email="user@example.com",
+    password=generate_password_hash("newpassword123"),
+    role="admin",
+    team=team,
+)
+db.session.add(new_admin)
+db.session.commit()
